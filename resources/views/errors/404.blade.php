f98a88993458ee62535413aabe147e3bb6fe265e
gananath-oss <ganaya2k17@gmail.com>
2024-09-13 07:45:33 +0400

    Complete Basic Laravel-11 project
    - Update Route
    - Add Auth and Validation using Breeze
    - Basic CRUD for notes application

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <div class="bg-gray-100 min-h-screen flex items-center justify-center">
+        <h1 class="text-gray-500 text-4xl font-sans">
+            Not Found | 404
+        </h1>
+    </div>
+</x-app-layout>
